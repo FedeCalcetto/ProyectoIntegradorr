1b2f4ad6d6c72bd825d187a56c639cc2ce2d750a
Facundo Cenoz Barneche <FC316704@fi365.ort.edu.uy>
2026-04-01 13:55:09 -0300

    cambiso en vistas, lenguaje, calificaion artesanos

--- ProyectoIntegrador_Web/Controllers/HomeController.cs
+++ ProyectoIntegrador_Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
+using Azure;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using ProyectoIntegrador_Web.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Localization;
 
 namespace ProyectoIntegrador_Web.Controllers
 {
@@ -28,5 +31,17 @@ namespace ProyectoIntegrador_Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+
+
+        public IActionResult CambiarIdioma(string culture, string returnUrl)
+            {
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                    new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+                return LocalRedirect(returnUrl);
+            }
+}
 }
